8d06751b88a831e0c78302d579f4033a5bd15b4b
Kingswithkings <kingsuthanaidogu@gmail.com>
2023-09-28 15:19:14 +0100

    Add project files.

---
+++ ApiDemoApp/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiDemoApp.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [MinLength(10)]
+        public string FirstName { get; set; }
+    }
+}
